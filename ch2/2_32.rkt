df11e51c2a63a1305e9bea18844b398dd7e504be
Birykov Egor <birykovegor@gmail.com>
2016-02-23 16:28:40 +0300

    Add explanation of 2.32

--- ch2/2_32.rkt
+++ ch2/2_32.rkt
@@ -1,6 +1,10 @@
 #lang racket
 (require "2_31.rkt")
 
+; subsets procedure
+; generates subsets of a given set s
+; first, it generates subsets for (cdr s) set
+; then, it appends result of a previous step with the same result, that 'consed' with (car s) via map
 (define (subsets s)
   (if (null? s)
       (list null)
