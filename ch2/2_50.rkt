e0638a2f4869097a74e374ca15c93f1cf75423da
Birykov Egor <birykovegor@gmail.com>
2016-03-01 20:45:25 +0300

    Solve 2.50

---
+++ ch2/2_50.rkt
@@ -0,0 +1,18 @@
+#lang racket
+(define (flip-horiz painter)
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+(define (rotate-180-cc painter)
+  (transform-painter painter
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 0.0)))
+
+(define (rotate-270-cc painter)
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
