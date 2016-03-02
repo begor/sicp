8c86826f5b5aac637d7032447216a576052f866b
Birykov Egor <birykovegor@gmail.com>
2016-03-02 19:16:45 +0300

    Solve 2.51

---
+++ ch2/2_51.rkt
@@ -0,0 +1,36 @@
+#lang racket
+(define (beside painter1 painter2)
+   (let ((split-point (make-vect 0.5 0.0)))
+   (let ((paint-left
+         (transform-painter painter1
+                            (make-vect 0.0 0.0)
+                            split-point
+                            (make-vect 0.0 1.0)))
+         (paint-right
+         (transform-painter painter2
+                            split-point
+                           (make-vect 1.0 0.0)
+                           (make-vect 0.5 1.0))))
+   (lambda (frame)
+     (paint-left frame)
+     (paint-right frame)))))
+
+(define (below-a p1 p2)
+  (let ((split-point (make-vect 0.0 0.5)))
+    (let ((paint-bottom
+           (transform-painter p1
+                              (make-vect 0.0 0.0)
+                              split-point
+                              (make-vect 1.0 0.0)))
+          (paint-top
+           (transform-painter painter2
+                              split-point
+                              (make-vect 0.0 1.0)
+                              (make-vect 1.0 0.5)))))
+    (lambda (frame)
+      (paint-bottom frame)
+      (paint-top frame))))
+
+(define (below-b painter1 painter2)
+   (rotate-90 (beside (rotate-270 painter1)
+                      (rotate-270 painter2))))
