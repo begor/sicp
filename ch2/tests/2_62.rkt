b7c093ae54078ccb71ebfe84471d69ebb66bc586
Birykov Egor <birykovegor@gmail.com>
2016-03-09 20:23:00 +0300

    Fix 2.62

--- ch2/tests/2_62.rkt
+++ ch2/tests/2_62.rkt
@@ -6,9 +6,9 @@
 (define ordered-set-tests
   (test-suite
    "Tests for exercise 2_62"
-   (check-equal? (union-set '(1 2 3 4 5) '(4 5 6 7)) '(4 5))
-   (check-equal? (union-set '(1 2 3) '(4 5 6)) '())
-   (check-equal? (union-set '(1 2 3) '(0 1 2 4 5 6)) '(1 2))))
+   (check-equal? (union-set '(1 2 3 4 5) '(4 5 6 7)) '(1 2 3 4 5 6 7))
+   (check-equal? (union-set '(1 2 3) '(4 5 6)) '(1 2 3 4 5 6))
+   (check-equal? (union-set '(1 2 3) '(0 1 2 4 5 6)) '(0 1 2 3 4 5 6))))
          
 
 (run-tests ordered-set-tests)
