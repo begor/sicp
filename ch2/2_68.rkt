1ed247b9122ff650cd5adfb78d4ef45874291ed9
Birykov Egor <birykovegor@gmail.com>
2016-03-12 18:38:08 +0300

    Solve 2.68. Refactoring nedeed.

---
+++ ch2/2_68.rkt
@@ -0,0 +1,34 @@
+#lang racket
+(require "2_67.rkt")
+
+(define (encode message tree)
+  (if (null? message)
+      '()
+      (append (encode-symbol (car message) tree)
+              (encode (cdr message) tree))))
+
+(define (encode-symbol symbol tree)
+  (define (in-branch? symbol branch)
+    (not (false? (memq symbol (symbols branch)))))
+  (define (encode acc tree)
+    (if (leaf? tree)
+        acc
+        (let ([left (left-branch tree)]
+              [right (right-branch tree)])
+          (cond ((in-branch? symbol left)
+                 (encode (append acc '(0)) left))
+                ((in-branch? symbol right)
+                 (encode (append acc '(1)) right))
+                (else (error "Символа нет в дереве!" symbol))))))
+  (encode '() tree))
+
+(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+                  (make-code-tree
+                   (make-leaf 'B 2)
+                   (make-code-tree (make-leaf 'D 1)
+                                   (make-leaf 'C 1)))))
+
+(provide encode)
+                       
+        
