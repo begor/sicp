18a22a7153806054cf19a1a812a05bcbf3b3bf92
Birykov Egor <birykovegor@gmail.com>
2016-03-31 20:59:53 +0300

    Fix 2.81

--- ch2/2_81.rkt
+++ ch2/2_81.rkt
@@ -1,19 +1,19 @@
 #lang racket
 (define (apply-generic op . args)
-  (let ((type-tags (map type-tag args)))
-    (let ((proc (get op type-tags)))
+  (let* ([type-tags (map type-tag args)]
+         [proc (get op type-tags)])
       (if proc
           (apply proc (map contents args))
           (if (= (length args) 2)
-              (let ((type1 (car type-tags))
-                    (type2 (cadr type-tags))
-                    (a1 (car args))
-                    (a2 (cadr args)))
+              (let ([type1 (car type-tags)]
+                    [type2 (cadr type-tags)]
+                    [a1 (car args)]
+                    [a2 (cadr args)])
                 (if (eq? type1 type2)
                     (error "Нет метода для этих типов"
                            (list op type-tags))
-                    (let ((t1->t2 (get-coercion type1 type2))
-                          (t2->t1 (get-coercion type2 type1)))
+                    (let ([t1->t2 (get-coercion type1 type2)]
+                          [t2->t1 (get-coercion type2 type1)])
                       (cond (t1->t2
                              (apply-generic op (t1->t2 a1) a2))
                             (t2->t1
@@ -22,4 +22,4 @@
                              (error "Нет метода для этих типов"
                                     (list op type-tags)))))))
               (error "Нет метода для этих типов"
-                     (list op type-tags)))))))
+                     (list op type-tags))))))
